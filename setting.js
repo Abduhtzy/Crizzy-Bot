fdbbfc25b1c573742b99f42e6851b2dcb8501cb1
Mabduhsrzyjs02917777dims <113650894+Mabduhsrzyjs02917777dims@users.noreply.github.com>
2022-10-14 01:31:05 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -10,18 +10,18 @@ global.Api = {
 global.ApiKey = 'coderevolution' //JIKA INGIN MENDAPATKAN KOMISI DAFTAR DI HTTPS://AMPIBISMM.MY.ID UNTUK MENDAPATKAN APIKEY!
 
 global.smm = {
-  nomor : '6283102650464',
-  atasnama : 'Ahmad Nur Fadhil'
+  nomor : '6283819140194',
+  atasnama : '𝔇𝔦𝔪𝔰'
 }
 
 // Other
-global.owner = ['6283102650464',"6282135724604"]
-global.premium = ['6283102650464']
-global.profilebio = ['6283102650464'] // ini ganti nomor lu ntar bisa nampilin pp lu
+global.owner = ['6283819140194',"6283819140194"]
+global.premium = ['6283866276094']
+global.profilebio = ['6283819140194'] // ini ganti nomor lu ntar bisa nampilin pp lu
 global.sponsor = 'no sponsors in:('
-global.packname = 'Crizzy-Bot'
-global.footer = '© 2022 Fadhil Graphy'
-global.author = 'Owner : Fadhil Graphy\nIg1 : @fdhlgrphy\nIg2 : @nr.fdhl__'
+global.packname = '𝘚𝘦𝘮𝘻'
+global.footer = '© 2022 𝔇𝔦𝔪𝔰'
+global.author = 'Owner : 𝔇𝔦𝔪𝔰\nIg1 : @fdhlgrphy\nIg2 : @nr.fdhl__'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.act = {
 		wait: 'please wait a moment, processing data...',
